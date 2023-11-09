827915302359ca012aa790868f70ee535133f3b3
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2023-11-09 09:37:22 +0200

    feat: set up frontend (#5)

---
+++ frontend/src/app/games/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-black">Page id</h1>
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+    return <h2 className="text-black">Home</h2>
+}
